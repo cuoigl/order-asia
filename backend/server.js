341ca342002c59896d5be398cc4909e38bf01287
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-12-29 22:04:29 +0700

    User Profile Endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -15,11 +18,16 @@ connectDB(); // Connect to MongoDB
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
