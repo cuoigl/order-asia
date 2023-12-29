4a0c504fa975cebfefd895b443c1d8dc08b511af
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-12-29 18:27:22 +0700

    Loader & Message Components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./assets/styles/bootstrap.custom.css";
-import "./assets/styles/index.css";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -26,7 +30,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
--- frontend/src/screens/HomeScreens.jsx
+++ frontend/src/screens/HomeScreens.jsx
@@ -1,33 +1,37 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
 import { Row, Col } from "react-bootstrap";
+
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 import { Banner } from "../components/Banner";
 import { Product } from "../components/Product";
 
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+
 export const HomeScreens = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
   return (
     <>
       <Banner />
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} xs={6} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} xs={6} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
