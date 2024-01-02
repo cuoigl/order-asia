d374e7929589daaede87d030ac039c6560a92579
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2024-01-02 18:42:01 +0700

    add banner done

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,25 +1,27 @@
 import Carousel from "react-bootstrap/Carousel";
-import banner from "../assets/banner.png";
+import banner1 from "../assets/banner1.png";
+import banner2 from "../assets/banner2.png";
+import banner3 from "../assets/banner3.png";
 
 export const Banner = () => {
   return (
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
-        <img className="d-block w-100" src={banner} alt="First slide" />
+        <img className="d-block w-100" src={banner1} alt="First slide" />
         <Carousel.Caption>
           {/* <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={banner} alt="Second slide" />
+        <img className="d-block w-100" src={banner2} alt="Second slide" />
         <Carousel.Caption>
           {/* <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={banner} alt="Third slide" />
+        <img className="d-block w-100" src={banner3} alt="Third slide" />
         <Carousel.Caption>
           {/* <h5>Third slide label</h5>
           <p>   
