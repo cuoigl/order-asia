951823e50005bd9a0e8c1185ce215958c65e4744
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2024-01-07 11:42:13 +0700

    Fix footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,17 +1,18 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 import { FaFacebookF } from "react-icons/fa6";
 import { FaTiktok } from "react-icons/fa";
 import { SiZalo } from "react-icons/si";
 import { FaYoutube } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import zaloQR from "../assets/qr_zalo.jpg";
 
 export const Footer = () => {
   return (
     <footer className="background-footer">
       <Container>
         <Row>
-          <Col className="py-3" lg={3} xl={6}>
+          <Col className="py-3" lg={3} xl={5}>
             <img src={logo} alt="" />
             <p>CÔNG TY TNHH THƯƠNG MẠI QUỐC TẾ ASIABUY</p>
             <p>
@@ -33,44 +34,54 @@ export const Footer = () => {
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </Col>
-          <Col lg={3} xl={2} className="py-3">
-            <div className="ms-2">
-              <div className="py-1">
-                <a href="https://www.facebook.com/asiabuy.logistics">
-                  <div className="icon-footer">
-                    <FaFacebookF />
+          <Col lg={3} xl={3} className="py-3">
+            <Row>
+              <Col>
+                <div className="ms-5 mt-2">
+                  <div className="py-1">
+                    <a href="https://www.facebook.com/asiabuy.logistics">
+                      <div className="icon-footer">
+                        <FaFacebookF />
+                      </div>
+                    </a>
                   </div>
-                </a>
-              </div>
-              <div className="py-1">
-                <a href="https://zalo.me/0796690609">
-                  <div className="icon-footer">
-                    <SiZalo />
+                  <div className="py-1">
+                    <a href="https://zalo.me/0796690609">
+                      <div className="icon-footer">
+                        <SiZalo />
+                      </div>
+                    </a>
                   </div>
-                </a>
-              </div>
-              <div className="py-1">
-                <a href="https://www.tiktok.com/@asiabuy1?is_from_webapp=1&sender_device=pc">
-                  <div className="icon-footer">
-                    <FaTiktok />
+                  <div className="py-1">
+                    <a href="https://www.tiktok.com/@asiabuy1?is_from_webapp=1&sender_device=pc">
+                      <div className="icon-footer">
+                        <FaTiktok />
+                      </div>
+                    </a>
                   </div>
-                </a>
-              </div>
-              <div className="py-1">
-                <a href="https://www.youtube.com/channel/UCFnn1X9_MkjTQFrQS6VnKkA">
-                  <div className="icon-footer">
-                    <FaYoutube />
+                  <div className="py-1">
+                    <a href="https://www.youtube.com/channel/UCFnn1X9_MkjTQFrQS6VnKkA">
+                      <div className="icon-footer">
+                        <FaYoutube />
+                      </div>
+                    </a>
                   </div>
-                </a>
-              </div>
-              <div className="py-1">
-                <a href="https://www.instagram.com/aisiabuy/">
-                  <div className="icon-footer">
-                    <FaInstagram />
+                  <div className="py-1">
+                    <a href="https://www.instagram.com/aisiabuy/">
+                      <div className="icon-footer">
+                        <FaInstagram />
+                      </div>
+                    </a>
                   </div>
-                </a>
-              </div>
-            </div>
+                </div>
+              </Col>
+              <Col>
+                <div className="mt-4">
+                  <h5>Zalo</h5>
+                  <Image src={zaloQR} rounded width="150px" />
+                </div>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
